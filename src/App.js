171c2ea9631aba95963cfc876e9368c114c31c81
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-16 21:30:34 -0700

    Completed Checkpoint 7
    Default Props and Class Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import List from  './List';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
+
+class App extends Compononent {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  }
+
+  render() {
+    const { store } = this.defaultProps
+    return (
+      <main className='App'>
+        <header className='App'>
+          <h1>Trelloyes!</h1>
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])} 
+              />
+          ))}
+        </div>
+      </main>
+    )
+  }
+};
+
 
 export default App;
